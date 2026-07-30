bd67b6ff07920b86c515adb1858194c90dcc98b3
Lneiras <lauraneira62@gmail.com>
2026-07-30 07:00:11 -0500

    first commit

---
+++ app/src/repositories/interfaces/user.repository.interface.ts
@@ -0,0 +1,25 @@
+// app/src/repositories/interfaces/user.repository.interface.ts
+
+import User, { UserCreationAttributes } from "../../models/user.model";
+
+/**
+ * Contrato del Repositorio de Usuarios
+ * -----------------------------------
+ * Define las operaciones de persistencia disponibles para la entidad User.
+ *
+ * Cualquier implementación deberá cumplir esta interfaz.
+ */
+
+export interface IUserRepository {
+
+    /**
+     * Crea un usuario.
+     */
+    create(data: UserCreationAttributes): Promise<User>;
+
+    /**
+     * Obtiene todos los usuarios.
+     */
+    findAll(): Promise<User[]>;
+
+}
