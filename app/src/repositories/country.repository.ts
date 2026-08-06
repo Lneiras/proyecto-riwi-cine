a1a0332840e24d03c33d1d8834141b86e22e5be6
JorgeCb12 <jorgeluiscorralesbarraza272@gmail.com>
2026-08-06 09:29:07 -0500

    actualizar el método findCountries para usar el repositorio

--- app/src/repositories/country.repository.ts
+++ app/src/repositories/country.repository.ts
@@ -3,7 +3,7 @@ import Country from "../models/country.model";
     
 class CountryRepository {
 
-    async findCountries(): Promise<any[]> {
+    async findCountries(): Promise<Country[]> {
 
         // Aquí deberías implementar la lógica para obtener los países desde la base de datos.
 
