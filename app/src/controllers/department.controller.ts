e1c9b1c76fae296921403fc9c89a5d0e2cce9604
Luyss264 <joseacevedovargas@gmail.com>
2026-08-09 20:25:08 -0500

    agregados departamentos y se hicieron correcciones en declaraciones

--- app/src/controllers/department.controller.ts
+++ app/src/controllers/department.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import departmentService from "../services/department.service";
+import DepartmentService from "../services/department.service";
 
 /**
  * Controlador para obtener la lista de departamentos.
@@ -12,7 +12,11 @@ export const getDepartments = async (_req: Request, res: Response): Promise<Resp
 
     try {
 
-        const departments = await departmentService.findDepartments();
+        const departments = await DepartmentService.getAllDepartments();
+
+        if (!departments) {
+            res.status(404).json({error: "There's no departments found"})
+        }
 
         return res.status(200).json(departments);
 
@@ -25,3 +29,22 @@ export const getDepartments = async (_req: Request, res: Response): Promise<Resp
     }
 
 };
+
+export const getDepartmentById = async(req: Request, res: Response): Promise<Response> => {
+    try {
+        const {id} = req.params;
+
+        const department = await DepartmentService.getDepartmentById(parseInt(id))
+
+        if (!department) {
+            res.status(404).json({error: "That city doesn't exist or is not available"})
+        }
+
+        return res.status(200).json(department)
+        
+    } catch (error: any) {
+        return res.status(500).json({
+            error: error.message
+        });
+    } 
+}
