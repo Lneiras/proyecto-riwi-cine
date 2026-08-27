c1b4d579aeda92a13c277a8fe8bdacd9ed08c56c
kevinusergt <kevinvilla6033@gmail.com>
2026-08-27 12:15:06 -0500

    Trabajando en Ticket controller, empezando por la funcionalidad y respuesta de generacion de ticket

---
+++ app/src/controllers/ticket.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import ticketService from "../services/ticket.service";
+
+export const generateTickets = async (req:Request, res: Response, next: NextFunction)=>{
+    try {
+        const userId = req.userId;
+        if (!userId) {
+            return res.status(401).json({
+                success: false,
+                error: "Sesión requerida",
+            });
+        };
+
+        const {reservationEntryIds} = req.body;
+
+        if (!Array.isArray(reservationEntryIds || reservationEntryIds.length === 0)) {
+            return res.status(400).json({
+                success: false,
+                error: "ReservationEntryIds debe ser un arreglo",
+            });
+        };
+
+        const result = await ticketService.generateTickets(userId, reservationEntryIds);
+
+        return res.status(201).json({
+            success: true,
+            data: result,
+        });
+
+    } catch (error:any) {
+        next(error);
+    }
+}
